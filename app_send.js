77af0be895d49bda2cca4abd89ec5284b602f363
Davor Cuvalo <adriatic111@yahoo.com>
2017-10-09 10:50:33 +0200

    GIT push pull

--- app_send.js
+++ app_send.js
@@ -21,3 +21,4 @@ app.listen(3000, function () {  // path je localhost ili IP 127.0.0.1
 
 // GIT Upload popravljenih i novih fajlova
 // git status, git add ., git commit -m "Now version" , git push (origin master)
+// Za povlačenje fajlova: git pull origin master
